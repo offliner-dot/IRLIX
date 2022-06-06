58f09ee96acadcdd1c27d2dec1960df38d08ae37
offliner-dot <rus7284631@gmail.com>
2022-06-06 20:18:52 +0300

    some change

--- on_react/src/components/Main/Form/Form.tsx
+++ on_react/src/components/Main/Form/Form.tsx
@@ -2,8 +2,10 @@ import React, {useState} from 'react';
 
 
 const Form = ({setListData, listData}: any) => {
-        const [numberFieldValue, setNumberFieldValue] = useState("")
-        const [textFieldValue, setTextFieldValue] = useState("")
+        const [numberFieldValue, setNumberFieldValue] = useState({name: "number", value: ""})
+        const [textFieldValue, setTextFieldValue] = useState({name: "text", value: ""})
+        const [isValidNumber, setIsValidNumber] = useState(false)
+        const [isValidText, setIsValidText] = useState(false)
 
         const handleSubmit = () => {
             if (listData.some((item: any) => item.id === numberFieldValue)) {
@@ -16,46 +18,73 @@ const Form = ({setListData, listData}: any) => {
                 setListData([...listData, {id: numberFieldValue, text: textFieldValue}]);
             }
         }
-        const onChangeNumber = (value: string) => {
-            setNumberFieldValue(value)
-
-            // let index = null;
-            // if (validateFieldNumber(number, numberFieldError) || validateFieldText(text, textFieldError)) {
-            //     validateFieldNumber(number, numberFieldError);
-            //     validateFieldText(text, textFieldError);
-            //     return;
-            // }
-            // for (let i = 0; i < listData.length; i++) {
-            //     if (listData[i].number === number) {
-            //         index = String(i);
-            //         break;
-            //     }
-            // }
-
-            // index ? listData[index].text = text : listData.push({number, text});
-            // listData.sort((a, b) => a.number - b.number);
-            // addListItems(listData);
+        const isValidField = (field: any) => {
+            switch (field.name) {
+                case    "number": {
+                    setNumberFieldValue(field.value);
+                    if (field.value === "") {
+                        setIsValidNumber(true);
+                        return "Поле обязательно для заполнения";
+                    } else if (isNaN(+field.value) || field.value < 1) {
+                        setIsValidNumber(true);
+                        return "Введены не корректные значения";
+                    } else {
+                        setIsValidNumber(false);
+                        return "";
+                    }
+                }
+                case "text": {
+                    setTextFieldValue(field.value);
+                    switch (field.value) {
+                        case "": {
+                            setIsValidText(true);
+                            return "Поле обязательно для заполнения";
+                        }
+                        default:
+                            setIsValidText(false);
+                            return "";
+                    }
+                }
+            }
         }
+        // let index = null;
+        // if (validateFieldNumber(number, numberFieldError) || validateFieldText(text, textFieldError)) {
+        //     validateFieldNumber(number, numberFieldError);
+        //     validateFieldText(text, textFieldError);
+        //     return;
+        // }
+        // for (let i = 0; i < listData.length; i++) {
+        //     if (listData[i].number === number) {
+        //         index = String(i);
+        //         break;
+        //     }
+        // }
+
+        // index ? listData[index].text = text : listData.push({number, text});
+        // listData.sort((a, b) => a.number - b.number);
+        // addListItems(listData);
         return (
             <form className="form" action="#" onSubmit={handleSubmit}>
                 <div className="form__number">
                     <label htmlFor="number">Номер записи</label>
                     <input type="text" id="number"
                            placeholder="Введите число"
-                           value={numberFieldValue}
-                           onChange={e => onChangeNumber(e.target.value)}
+                           value={numberFieldValue.value}
+                           onChange={e => isValidField({name:"number",value:e.target.value})}
+                           name="number"
                     />
-                    {validateFieldNumber(numberFieldValue) && <span
-                        className="numberFieldError">{validateFieldNumber(numberFieldValue)}</span>}
+                    {isValidNumber && <span
+                        className="numberFieldError">{isValidField(numberFieldValue)}</span>}
                 </div>
                 <div className="form__text">
                     <label htmlFor="text">Название записи</label>
                     <input type="text" id="text"
                            placeholder="Введите текст"
-                           value={textFieldValue}
-                           onChange={e => setTextFieldValue(e.target.value)}
+                           value={textFieldValue.value}
+                           onChange={e => isValidField({name:"text",value:e.target.value})}
+                           name="text"
                     />
-                    <span className="textFieldError"></span>
+                    {isValidText && <span className="textFieldError">isValidField(textFieldValue)</span>}
                 </div>
                 <button className="form__button">
                     Добавить в список
